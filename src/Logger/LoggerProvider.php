2632698fa078b8f48ea6edb2243c9f890280167d
Mohamadreza <mreza@Mohamadrezas-MacBook-Pro.local>
2022-12-27 14:04:52 +0100

    add Logger provider class

---
+++ src/Logger/LoggerProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Waxwink\Orbis\Logger;
+
+use Waxwink\Orbis\Configuration\ConfigurationInterface;
+use Waxwink\Orbis\Contracts\Bootable;
+use Waxwink\Orbis\Contracts\ContainerInterface;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\ResettableInterface;
+use Psr\Log\LoggerInterface;
+
+class LoggerProvider implements Bootable
+{
+    public function __construct(protected ContainerInterface $container, protected ConfigurationInterface $configuration)
+    {
+    }
+
+    public function boot(): void
+    {
+        if ($this->configuration->getEnv() === 'test') {
+            return;
+        }
+
+        $path = $this->configuration->get("app.log.path") ?? $this->configuration->getBasePath() . "/var/logs/";
+        $path .= "/logs.log";
+        $logger = new Logger('storage');
+        $logger->pushHandler(new StreamHandler(removeDuplicateSlashes($path), Level::Warning));
+
+        $this->container->set(LoggerInterface::class, $logger);
+        $this->container->set(ResettableInterface::class, $logger);
+    }
+}
